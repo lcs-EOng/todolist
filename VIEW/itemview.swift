05620d49aa2e6f62828549e1a124ed67191f0d31
Eason Ong <eong@lcs.on.ca>
2024-04-11 15:08:14 -0400

    Made to-do items editable and responsive to size of the text they contain.

---
+++ VIEW/itemview.swift
@@ -0,0 +1,37 @@
+//
+//  itemview.swift
+//  todolist
+//
+//  Created by Ong Eason on 9/4/2024.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    @Binding var currentItem: TodoItem
+    
+    var body: some View {
+        Label(title: {
+            TextField("Enter a to-do item", text: $currentItem.title, axis: .vertical)
+        },
+              icon: {
+            Image(
+                systemName:currentItem.done == true ? "checkmark.circle" : "circle"
+            )
+            // Tap to mark as done
+                .onTapGesture {
+                    currentItem.done.toggle()
+                }
+        }
+              
+        )
+    }
+}
+
+#Preview {
+    List{
+        ItemView(currentItem: Binding.constant(firstItem))
+        ItemView(currentItem: Binding.constant(secondItem))
+    }
+    
+}
